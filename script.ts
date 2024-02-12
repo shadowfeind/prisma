59b0ab5730e43ba94da2738a4539a57dcfb26a36
shadowfeind <travestroy007@gmail.com>
2024-02-12 16:35:20 +0545

    exploring all prisma datatypes and relationship

---
+++ script.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+// const main = async() => {
+//    await prisma.user.deleteMany()
+// }
+
+// try {
+//     main()
+// } catch (error) {
+//     console.log(error)
+// } 
